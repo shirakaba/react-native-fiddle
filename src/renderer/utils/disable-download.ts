1903051d79d32ec71e4c9348b2f96bb4c1492d79
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-11-02 14:48:54 +0900

    fix disableDownload() logic

--- src/renderer/utils/disable-download.ts
+++ src/renderer/utils/disable-download.ts
@@ -1,20 +1,15 @@
-import semver from 'semver';
-
 /**
  * disables download button for versions:
- * - below 11.0.0 on Apple Silicon.
- * - below 6.0.8 and 7.0.0 on Windows arm64
- * Reference: {@link https://www.electronjs.org/blog/apple-silicon}
+ * - on all non-ARM Macs.
+ * - on all Windows PCs.
+ * Reference: {@link https://github.com/shirakaba/rnmprebuilds/releases}
  *
- * @param version - electron version
+ * @param _version - electron version
  */
-export function disableDownload(version: string): boolean {
+export function disableDownload(_version: string): boolean {
   return (
     (window.ElectronFiddle.platform === 'darwin' &&
-      window.ElectronFiddle.arch === 'arm64' &&
-      semver.lt(version, '11.0.0')) ||
-    (window.ElectronFiddle.platform === 'win32' &&
-      window.ElectronFiddle.arch === 'arm64' &&
-      !semver.satisfies(version, '>=6.0.8 || >=7.0.0'))
+      window.ElectronFiddle.arch !== 'arm64') ||
+    window.ElectronFiddle.platform === 'win32'
   );
 }
