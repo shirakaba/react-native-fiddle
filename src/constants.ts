3e74b02c0d6f2c4226c529619322b06a41995456
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-12-06 10:31:24 +0900

    set up Sentry

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SENTRY_DSN =
-  'https://966a5b01ac8d4941b81e4ebd0ab4c991@sentry.io/1882540';
+  'https://1df9bc34560bf2273e6d15b6c9277f68@o4510485292843008.ingest.de.sentry.io/4510485296054352';
 
 export const ELECTRON_DTS = 'electron.d.ts';
