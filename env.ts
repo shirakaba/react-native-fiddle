7a35f180b1f4079a91336a3a81c8912c5610f61f
shirakaba <14055146+shirakaba@users.noreply.github.com>
2025-12-13 18:36:38 +0900

    handle missing .env file

--- env.ts
+++ env.ts
@@ -2,5 +2,15 @@ import * as path from 'node:path';
 
 export function sourceEnvVars() {
   const envFilePath = path.resolve(__dirname, './.env');
-  process.loadEnvFile(envFilePath);
+  try {
+    process.loadEnvFile(envFilePath);
+  } catch (error) {
+    if (
+      !(error instanceof Error) ||
+      !('code' in error) ||
+      error.code !== 'ENOENT'
+    ) {
+      throw error;
+    }
+  }
 }
